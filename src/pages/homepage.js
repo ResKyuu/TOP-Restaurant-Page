d3f38542cf505daa10f87af26396a7ad52a7bb5f
ResKyuu <reskyuuotaku@gmail.com>
2025-05-27 16:07:56 +0200

    Continued the pseudo design of the homepage

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,16 +1,22 @@
-import mainImage from "../images/Gemini_Generated_Image_ds5p19ds5p19ds5p.jpeg";
+import mainImage from "../images/779246.jpg";
 
 function loadHomepage() {
-    const mainContent = document.querySelector("#content");
-    const homePageImage = document.createElement("img");
+  const mainContent = document.querySelector("#content");
+  const homePageImage = document.createElement("img");
+  const homePageContent = document.createElement("div");
+  const homePageTitle = document.createElement("h1");
+  const homePageText = document.createElement("p");
 
-    //create the main image for the homepage
-    homePageImage.src = mainImage;
-    homePageImage.classList.add("homePageImage")
-    mainContent.appendChild(homePageImage);
-    
+  homePageContent.classList.add("homePageContent");
+  mainContent.appendChild(homePageContent);
+
+  homePageTitle.textContent = "Craving for a delicious Kebab?";
+  homePageContent.appendChild(homePageTitle);
+
+  //create the main image for the homepage
+  homePageImage.classList.add("homePageImage");
+  homePageImage.src = mainImage;
+  mainContent.appendChild(homePageImage);
 }
-
-
 
 export { loadHomepage };
